b2cc7968e19b829e0c61c0886b31489d05578912
Joao Zsigmond <jzsiggy@gmail.com>
2019-11-21 21:35:55 -0200

    Adding the createUser controller

---
+++ src/controllers/authenticationControllers/createUser.js
@@ -0,0 +1,44 @@
+const { User } = require("../../models");
+const bcrypt = require("bcrypt")
+
+const createUser = (request, response, next) => {
+  const newUserObject = request.body;
+
+  if (newUserObject.password != newUserObject.passwordConfirm) {
+    response.render("signup", {
+      errorMessage : "Password confirmation does not match password",
+    });
+    return null;
+  };
+
+  User.find({ username : newUserObject.username })
+  .then(existingUser => {
+    console.log(existingUsers);
+    if (existingUsers.length) {
+      response.render("signup", {
+        errorMessage : "Username already in use"
+      });
+    } else {
+      newUserHashPass = bcrypt.hashSync(newUserObject.password, 10);
+      User.create({
+        email : newUserObject.email,
+        username : newUserObject.username,
+        password : newUserHashPass,
+      })
+      .then(newUser => {
+        console.log(newUser);
+        response.redirect("/");
+      })
+      .catch(err => {
+        console.log("Error registering user to DB: ", err);
+        response.render('signup', {
+          errorMessage : "Error registering user to database",
+        });
+      });
+    };
+  })
+};
+
+module.exports = {
+  createUser,
+};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,8 @@
 const { renderHome , renderSignupForm } = require("./pagesController");
+const { createUser } = require("./authenticationControllers");
 
 module.exports = {
   renderHome,
   renderSignupForm,
+  createUser,
 };
