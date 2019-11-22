c0d886d90b0446d0b26482f68ca62498c844bd8f
Joao Zsigmond <jzsiggy@gmail.com>
2019-11-22 10:58:30 -0200

    Adding login routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
-const { renderHome , renderSignupForm , createUser } = require("../controllers");
+const { renderHome , renderSignupForm , renderLoginForm , createUser , loginUser } = require("../controllers");
 
 const express = require('express');
 const router = express.Router();
 
 router.get("/", renderHome);
+router.get("/login", renderLoginForm);
+router.post("/login", loginUser);
 router.get("/signup", renderSignupForm);
 router.post("/signup", createUser);
 
