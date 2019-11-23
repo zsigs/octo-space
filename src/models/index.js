b26219915d239545d18d4c4c809295b9144ae809
Joao Zsigmond <jzsiggy@gmail.com>
2019-11-23 14:20:47 -0200

    Adding nodemailer and confirmation email

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,9 @@
-const { Like , User } = require("./userModels");
+const { Like , User , EmailVerification } = require("./userModels");
 const { Movie } = require("./contentModels");
 
 module.exports = {
   Like,
   User,
+  EmailVerification,
   Movie,
 };
---
+++ src/resources/sendMail.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+const sendMail = async (recipient, subject, message) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass // generated ethereal password
+    }
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: recipient, // list of receivers
+    subject: subject, // Subject line
+    text: message, // plain text body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+module.exports = {
+  sendMail,
+};
