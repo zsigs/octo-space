14415d0a7e0c4c0a9db82750da6943861610a5df
Joao Zsigmond <jzsiggy@gmail.com>
2019-11-23 10:14:35 -0200

    Adding logout route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,19 @@
-const { renderHome , renderSignupForm , renderLoginForm , createUser , loginUser } = require("../controllers");
+const { renderHome , renderSignupForm , renderLoginForm , createUser , loginUser , logoutUser } = require("../controllers");
 
 const express = require('express');
 const router = express.Router();
+
+router.use((request, response, next) => {
+  console.log(request.session);
+  next();
+});
 
 router.get("/", renderHome);
 router.get("/login", renderLoginForm);
 router.post("/login", loginUser);
 router.get("/signup", renderSignupForm);
 router.post("/signup", createUser);
+router.get('/logout', logoutUser);
 
 module.exports = {
   router,
