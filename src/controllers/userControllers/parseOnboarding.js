3b96f22dd7c43c31d01e7db93e58ab7823a9a9e1
Joao Zsigmond <jzsiggy@gmail.com>
2019-12-05 21:30:54 -0200

    Onboarding backend

--- src/controllers/userControllers/parseOnboarding.js
+++ src/controllers/userControllers/parseOnboarding.js
@@ -1,8 +1,19 @@
-const parseOnboarding = (request, response, next) => {
-  const interests = request.body;
-  console.log(interests);
+const { User } = require('../../models')
 
-  response.render('/');
+const parseOnboarding = async (request, response, next) => {
+  const currentUser = request.session.user;
+  const interests = Object.keys(request.body);
+
+  const updatedUser = await User.findByIdAndUpdate(
+    currentUser._id,
+    { interests }, 
+    { new : true },
+  );
+
+  request.session.user = updatedUser;
+  console.log(updatedUser);
+
+  response.redirect('/');
 };
 
 module.exports = {
