0436b671227bfe02c4a7ab2085735914ec5bdda1
Joao Zsigmond <jzsiggy@gmail.com>
2019-12-05 20:03:48 -0200

    Init Onboarding

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const {
   renderHome,
   renderSignupForm,
   renderLoginForm,
+  renderOnboarding,
   createUser,
   loginUser,
   userLoggedIn,
   logoutUser,
   confirmUser,
+  parseOnboarding,
 } = require('../controllers');
 
 const express = require('express');
@@ -24,6 +26,8 @@ router.get("/signup", renderSignupForm);
 router.post("/signup", createUser);
 router.get('/logout', logoutUser);
 router.get('/confirm/:verificationId', confirmUser);
+router.get('/onboarding', renderOnboarding);
+router.post('/onboarding', parseOnboarding);
 
 module.exports = {
   router
