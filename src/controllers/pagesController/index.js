01f229854953b3c30f5cd25b1418c3cf1d7f78c4
Joao Zsigmond <jzsiggy@gmail.com>
2019-12-07 15:18:15 -0200

    Fix to onboarding

--- src/controllers/pagesController/index.js
+++ src/controllers/pagesController/index.js
@@ -2,10 +2,13 @@ const { renderHome } = require('./renderHome');
 const { renderSignupForm } = require('./renderSignupForm');
 const { renderLoginForm } = require('./renderLoginForm');
 const { renderOnboarding } = require('./renderOnboarding');
+const { renderTopicSection } = require('./renderTopicSection');
+
 
 module.exports = {
   renderHome,
   renderSignupForm,
   renderLoginForm,
   renderOnboarding,
+  renderTopicSection,
 };
--- src/controllers/userControllers/parseOnboarding.js
+++ src/controllers/userControllers/parseOnboarding.js
@@ -2,12 +2,41 @@ const { User } = require('../../models')
 
 const parseOnboarding = async (request, response, next) => {
   const currentUser = request.session.user;
-  const interests = Object.keys(request.body);
+
+  console.log(request.body) 
+
+  let articleInterests = request.body.article;
+  let songInterests = request.body.song;
+  let bookInterests = request.body.book;
+  let movieInterests = request.body.movie;
+
+  if (! articleInterests) {
+    articleInterests = [];
+  };
+
+  if (! songInterests) {
+    songInterests = [];
+  };
+
+  if (! bookInterests) {
+    bookInterests = [];
+  };
+
+  if (! movieInterests) {
+    movieInterests = [];
+  };
 
   const updatedUser = await User.findByIdAndUpdate(
     currentUser._id,
-    { interests }, 
-    { new : true },
+    {
+      articleInterests,
+      songInterests,
+      bookInterests,
+      movieInterests,
+    },
+    {
+      new : true
+    },
   );
 
   request.session.user = updatedUser;
