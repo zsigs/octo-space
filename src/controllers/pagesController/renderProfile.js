9def06d3eacead48fc14e84e9ae0fac0bcaa302b
Gabriel Zsigmond <gaazsig@gmail.com>
2019-12-10 22:04:29 -0300

    sends more info to user profile from backend to ease follow and unfollow

--- src/controllers/pagesController/renderProfile.js
+++ src/controllers/pagesController/renderProfile.js
@@ -1,15 +1,29 @@
-const { User } = require('../../models');
+const { User, Follow } = require('../../models');
 
 const renderProfile = (request, response, next) => {
   const { username } = request.params;
-  let showEdit = false
+  let showEdit = false;
+  let canFollow = true;
   console.log('username: ', username);
   User.findOne({ username })
     .then(res => {
       searchedUser = res.username;
       searchedEmail = res.email;
-      showEdit = searchedUser == request.session.user.username ? true : false
-      response.render('user/profile', { searchedUser, searchedEmail, showEdit });
+      id = res._id
+      console.log("id: ", id)
+      showEdit = searchedUser == request.session.user.username ? true : false;
+      Follow.findOne({ followerId: request.session.user._id, followingId: id }).then(following => {
+        console.log(following);
+        if (following){
+          canFollow = false
+        }
+        response.render('user/profile', {
+          searchedUser,
+          searchedEmail,
+          showEdit,
+          canFollow
+        });
+      });
     })
     .catch(err => {
       console.log(err);
